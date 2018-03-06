3b93c7fe2ef1c1406b0402116fbfd6a5be6f1558
unknown <quameu@gmail.com>
2018-03-07 00:55:19 +0300

    Added urls for yandex and ofd1

--- src/main/kotlin/com/github/shiverawe/Requests.kt
+++ src/main/kotlin/com/github/shiverawe/Requests.kt
@@ -12,4 +12,17 @@ object Requests{
     fun ofdRu(cc: ChequeCredentials):String {
         return """https://ofd.ru/api/rawdoc/RecipeInfo?Fn=${cc.fn}&Kkt=${cc.kkt}&Inn=${cc.inn}&Num=${cc.fd}&Sign=${cc.fp}"""
     }
+
+    fun _1_ofdRu(cc: ChequeCredentials): String {
+        return ""
+        // TODO VShefer 07 03 : Investigate how to use it
+        val url_first_get = "https://consumer.1-ofd.ru/#/landing"
+        val url_receipt_get = "https://consumer.1-ofd.ru/api/tickets/ticket/{}"
+        val url_receipt_find = "https://consumer.1-ofd.ru/api/tickets/find-ticket"
+    }
+
+
+    fun ofdya(cc: ChequeCredentials): String {
+        return """https://ofd-ya.ru/getFiscalDoc?kktRegId=${cc.kkt}&fiscalSign=${cc.fp}&json=true"""
+    }
 }
