de2b508d2cd4ea9a91883823bc45f916d7adc00c
ShiverAwe <quameu@gmail.com>
2018-03-28 00:40:12 +0300

    Updated Cheques dsl

--- src/main/kotlin/com/github/shiverawe/cheque/database/dsl/Cheques.kt
+++ src/main/kotlin/com/github/shiverawe/cheque/database/dsl/Cheques.kt
@@ -4,9 +4,14 @@ import org.jetbrains.exposed.sql.Table
 
 object Cheques : Table() {
     val id = integer("id").primaryKey().autoIncrement()
+    val inn = varchar("inn", 50)
+    val kkt = varchar("kkt", 50)
     val ofd = varchar("ofd", length = 50)
     val fn = varchar("fn", length = 50)
     val fd = varchar("fd", length = 50)
     val fp = varchar("fp", length = 50)
+    val shiftNumber = integer("shiftNumber")
+    val shiftDocNumber = integer("shiftDocNumber")
     val date = varchar("date", length = 50)
+    val summary = integer("summary")
 }
