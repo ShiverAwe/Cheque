e12066d2d76d3633cb10c13a74c123925d68bb41
ShiverAwe <quameu@gmail.com>
2018-03-31 14:33:43 +0300

    Created interface ChequeItemDao

---
+++ src/main/kotlin/com/github/shiverawe/cheque/ChequeItem.kt
@@ -0,0 +1,8 @@
+package com.github.shiverawe.cheque
+
+class ChequeItem(
+        val id: Int,
+        val chequeId: Int,
+        val name: String,
+        val cost: Int
+)
---
+++ src/main/kotlin/com/github/shiverawe/cheque/database/dao/ChequeItemDao.kt
@@ -0,0 +1,24 @@
+package com.github.shiverawe.cheque.database.dao
+
+import com.github.shiverawe.cheque.ChequeItem
+
+
+interface ChequeItemDao {
+
+    /**
+     * Returns list of cheque items ids, which belobg to cheque with specified chequeId,
+     * or empty list, if cheque id does not exist
+     */
+    fun getChequeItems(chequeId: Int): List<Int>
+
+    /**
+     * Creates a new cheque item
+     * Id, specified in ChequeItem could be ignored and selected by database
+     */
+    fun create(item: ChequeItem)
+
+    /**
+     * Deletes a cheque item by id
+     */
+    fun delete(id: Int)
+}
