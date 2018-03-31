e02bf08ea567524fe25513f9da4c3c44840eaa70
ShiverAwe <quameu@gmail.com>
2018-03-31 14:39:44 +0300

    Created interface CategoryDao

---
+++ src/main/kotlin/com/github/shiverawe/cheque/database/dao/CategoryDao.kt
@@ -0,0 +1,10 @@
+package com.github.shiverawe.cheque.database.dao
+
+interface CategoryDao {
+    /**
+     * Returns path from root category to current from category tree
+     */
+    fun getCategoryLine(name: String): List<String>
+
+    fun createCategory(name: String, parent: String)
+}
